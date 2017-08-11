626727d8e91413d71f5a0a69fc4ec203f1eae2e3
Kostiantyn Bondarenko <ko6tia@gmail.com>
2017-08-11 08:53:16 +0300

    Add JPA

--- src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
@@ -6,6 +6,8 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class DateTimeUtil {
+    public static final LocalDate MIN_DATE = LocalDate.of(1, 1, 1);
+    public static final LocalDate MAX_DATE = LocalDate.of(3000, 1, 1);
 
     private DateTimeUtil() {
     }
--- src/main/java/ru/javawebinar/topjava/util/ValidationUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/ValidationUtil.java
@@ -1,6 +1,6 @@
 package ru.javawebinar.topjava.util;
 
-import ru.javawebinar.topjava.model.BaseEntity;
+import ru.javawebinar.topjava.model.AbstractBaseEntity;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 public class ValidationUtil {
@@ -9,11 +9,11 @@ public class ValidationUtil {
     }
 
     public static void checkNotFoundWithId(boolean found, int id) {
-        checkNotFound(found, "getID=" + id);
+        checkNotFound(found, "id=" + id);
     }
 
     public static <T> T checkNotFoundWithId(T object, int id) {
-        return checkNotFound(object, "getID=" + id);
+        return checkNotFound(object, "id=" + id);
     }
 
     public static <T> T checkNotFound(T object, String msg) {
@@ -27,17 +27,17 @@ public class ValidationUtil {
         }
     }
 
-    public static void checkNew(BaseEntity entity) {
+    public static void checkNew(AbstractBaseEntity entity) {
         if (!entity.isNew()) {
-            throw new IllegalArgumentException(entity + " must be new (getID=null)");
+            throw new IllegalArgumentException(entity + " must be new (id=null)");
         }
     }
 
-    public static void assureIdConsistent(BaseEntity entity, int id) {
+    public static void assureIdConsistent(AbstractBaseEntity entity, int id) {
         if (entity.isNew()) {
             entity.setId(id);
         } else if (entity.getId() != id) {
-            throw new IllegalArgumentException(entity + " must be with getID=" + id);
+            throw new IllegalArgumentException(entity + " must be with id=" + id);
         }
     }
 }
